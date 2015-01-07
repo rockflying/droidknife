554f533336b4eaf12090c67b6cb454902ece3a2f
xuhao <hao.xu@cs2c.com.cn>
2015-01-07 16:46:40 +0800

    add file

---
+++ src/droid/knife/DroidKnife.java
@@ -0,0 +1,26 @@
+package droid.knife;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DroidKnife {
+	
+	String apk;
+	List<String> permissions = new ArrayList<String>();
+	
+	public DroidKnife(String apkname) {
+		apk = apkname;
+	}
+	
+	public void parseManifest() {
+		
+	}
+	
+	public void getPermissions() {
+		
+	}
+	
+	public void getComponents() {
+		
+	}
+}
